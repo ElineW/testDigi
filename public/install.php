0c8f2e5391c3210d818963444afd31522879fe6a
Eline Westerberg <elinewes@gmail.com>
2018-06-01 21:23:10 +0200

    fdnvjf

--- public/install.php
+++ public/install.php
@@ -3,7 +3,20 @@
 require "config.php";
 try {
 	$connection = new PDO("mysql:host=$host", $username, $password, $options);
-	$sql = file_get_contents(__DIR__."/data/init.sql");
+	$sql = "CREATE DATABASE test;
+
+	use test;
+	
+	CREATE TABLE users (
+		id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+		firstname VARCHAR(30) NOT NULL,
+		lastname VARCHAR(30) NOT NULL,
+		email VARCHAR(50) NOT NULL,
+		tlf VARCHAR(20),
+		fdato VARCHAR(50),
+		date TIMESTAMP
+	);";
+	
 	$connection->exec($sql);
 	
 	echo "Database and table users created successfully.";
