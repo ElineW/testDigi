201064b35ac9a62e63b9691918291b398a98560f
Eline Westerberg <elinewes@gmail.com>
2018-06-01 10:14:22 +0200

    testing

--- web/index.php
+++ web/index.php
@@ -1,6 +1,38 @@
 <?php
 
 require('../vendor/autoload.php');
+include "views/header.php"; 
+include "views/index.html";
+
+<ul>
+	<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
+	<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
+</ul>
+ ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+
+	<title>Simple Database App</title>
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<h1>Simple Database App</h1>
+
+	<ul>
+		<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
+		<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
+	</ul>
+
+</body>
+</html>
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
