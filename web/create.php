168502f6b60bbca30935eeba012d90d3778533a4
Eline Westerberg <elinewes@gmail.com>
2018-06-01 12:03:08 +0200

    endr

--- web/create.php
+++ web/create.php
@@ -1,34 +1,65 @@
-<?php include "templates/header.php"; ?>
-<?php include "templates/footer.php"; ?>
-
 <?php
-if (isset($_POST['submit'])) {}
-    if (isset($_POST['submit'])) {
-        require "../config.php";
-    
-        try {
-            $connection = new PDO($dsn, $username, $password, $options);
 
-$new_user = array(
-	"firstname" => $_POST['firstname'],
-	"lastname"  => $_POST['lastname'],
-	"email"     => $_POST['email'],
-	"tlf"       => $_POST['tlf'],
-	"fdato"  => $_POST['fdato']
-);
+/**
+ * Use an HTML form to create a new entry in the
+ * users table.
+ *
+ */
 
-$sql = sprintf(
-		"INSERT INTO %s (%s) values (%s)",
-		"users",
-		implode(", ", array_keys($new_user)),
-		":" . implode(", :", array_keys($new_user))
-);
 
-$statement = $connection->prepare($sql);
-$statement->execute($new_user);
-        } catch(PDOException $error) {
-            echo $sql . "<br>" . $error->getMessage();
-        };
-        /*<a href="index.php">Tilbake</a> **/
-    
+if (isset($_POST['submit'])) {
+	require "../config.php";
+	require "../common.php";
+
+	try {
+		$connection = new PDO($dsn, $username, $password, $options);
+		
+		$new_user = array(
+			"firstname" => $_POST['firstname'],
+			"lastname"  => $_POST['lastname'],
+			"email"     => $_POST['email'],
+			"age"       => $_POST['age'],
+			"location"  => $_POST['location']
+		);
+
+		$sql = sprintf(
+				"INSERT INTO %s (%s) values (%s)",
+				"users",
+				implode(", ", array_keys($new_user)),
+				":" . implode(", :", array_keys($new_user))
+		);
+		
+		$statement = $connection->prepare($sql);
+		$statement->execute($new_user);
+	} catch(PDOException $error) {
+		echo $sql . "<br>" . $error->getMessage();
+	}
+	
+}
 ?>
+
+<?php require "templates/header.php"; ?>
+
+<?php if (isset($_POST['submit']) && $statement) { ?>
+	<blockquote><?php echo $_POST['firstname']; ?> successfully added.</blockquote>
+<?php } ?>
+
+<h2>Add a user</h2>
+
+<form method="post">
+	<label for="firstname">Fornavn</label>
+	<input type="text" name="firstname" id="firstname">
+	<label for="lastname">Etternavn</label>
+	<input type="text" name="lastname" id="lastname">
+	<label for="email">Email Addresse</label>
+	<input type="text" name="email" id="email">
+	<label for="tlf">Telefonnummer</label>
+	<input type="text" name="tlf" id="tlf">
+	<label for="fdato">Fødselsdato</label>
+	<input type="text" name="fdato" id="fdato">
+	<input type="submit" name="submit" value="Submit">
+</form>
+
+<a href="index.php">Back to home</a>
+
+<?php require "templates/footer.php"; ?>
