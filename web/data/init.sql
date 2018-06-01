3c1d70b3682ce54aeed71867fd10055de9f3f019
Eline Westerberg <elinewes@gmail.com>
2018-06-01 11:07:27 +0200

    dbcon

--- web/data/init.sql
+++ web/data/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
 	firstname VARCHAR(30) NOT NULL,
 	lastname VARCHAR(30) NOT NULL,
 	email VARCHAR(50) NOT NULL,
-	age INT(3),
-	location VARCHAR(50),
+	tlf VARCHAR(20),
+	fdato VARCHAR(50),
 	date TIMESTAMP
 );
