a786d5e314201f00162ea88ed0c4d160fbac57df
Eline Westerberg <elinewes@gmail.com>
2018-06-01 10:05:11 +0200

    dhcsdhv

--- web/index.php
+++ web/index.php
@@ -40,18 +40,4 @@ $app['debug'] = true;
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-;
-
 $app->run();
