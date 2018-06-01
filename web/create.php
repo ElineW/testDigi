0a99bc64cd073b37b9ceb13f5c7236f2599dc252
Eline Westerberg <elinewes@gmail.com>
2018-06-01 11:21:32 +0200

    f

--- web/create.php
+++ web/create.php
@@ -1,17 +1,35 @@
 <?php include "templates/header.php"; ?>
 <?php include "templates/footer.php"; ?>
-<form method="post">
-	<label for="firstname">Fornavn</label>
-	<input type="text" name="firstname" id="firstname">
-	<label for="lastname">Etternavn</label>
-	<input type="text" name="lastname" id="lastname">
-	<label for="email">Email Addresse</label>
-	<input type="text" name="email" id="email">
-	<label for="tlf">Telefonnummer</label>
-	<input type="text" name="tlf" id="tlf">
-	<label for="fdato">Fødselsdato</label>
-    <input type="text" name="fdato" id="fdato">
-	<input type="submit" name="submit" value="Submit">
-</form>
 
-<a href="index.php">Tilbake</a>
+<?php
+if (isset($_POST['submit'])) {}
+    if (isset($_POST['submit'])) {
+        require "../config.php";
+    
+        try {
+            $connection = new PDO($dsn, $username, $password, $options);
+
+$new_user = array(
+	"firstname" => $_POST['firstname'],
+	"lastname"  => $_POST['lastname'],
+	"email"     => $_POST['email'],
+	"tlf"       => $_POST['tlf'],
+	"fdato"  => $_POST['fdato']
+);
+
+$sql = sprintf(
+		"INSERT INTO %s (%s) values (%s)",
+		"users",
+		implode(", ", array_keys($new_user)),
+		":" . implode(", :", array_keys($new_user))
+);
+
+$statement = $connection->prepare($sql);
+$statement->execute($new_user);
+        } catch(PDOException $error) {
+            echo $sql . "<br>" . $error->getMessage();
+        }
+        <a href="index.php">Tilbake </a>
+    };
+?>
+
