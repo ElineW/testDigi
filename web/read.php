7ced82f989404a70efeab00f461918aa93daa171
Eline Westerberg <elinewes@gmail.com>
2018-06-01 10:35:05 +0200

    les

---
+++ web/read.php
@@ -0,0 +1,11 @@
+<?php include "templates/header.php"; ?>
+<h2>Hent personopplysninger</h2>
+
+<form method="post">
+	<label for="firstname">Fornavn</label>
+	<input type="text" id="firstname" name="firstname">
+	<input type="submit" name="submit" value="View Results">
+</form>
+
+<a href="index.php">Tilbake</a>
+<?php include "templates/footer.php"; ?>
